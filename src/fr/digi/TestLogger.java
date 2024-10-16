339f25256e607fd76a136846d4f060febc41c6fa
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-16 13:42:31 +0200

    first commit

--- src/fr/digi/TestLogger.java
+++ src/fr/digi/TestLogger.java
@@ -1,4 +1,12 @@
-package fr.epsi;
+package fr.digi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestLogger {
+    private static final Logger LOGGER = LoggerFactory.getLogger( TestLogger.class );
+    public static void main(String[] args) {
+        LOGGER.info("Bonjour slf4J!");
+        LOGGER.info("Implémentation logback");
+
+    }
 }
